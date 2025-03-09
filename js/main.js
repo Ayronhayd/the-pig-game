d04e9be9da7bf16f574eafd63aea7658b72ef4b3
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-03-09 20:00:50 +0300

    0.0.1 js модули Раскидал файлы по папкам Проверка Готово к началу работы

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import randomNumber from './utils/randomNumber.js';
+
+randomNumber();
---
+++ js/utils/randomNumber.js
@@ -0,0 +1,7 @@
+const randomNumber =( ) => {
+    console.log('hello');
+    console.log(1 + 1);
+
+}
+
+export default randomNumber
